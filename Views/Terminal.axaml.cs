aff72071f1d904a3cb3c3b158a1506d0feb871d8
paul <paul@MAINPC>
2026-07-23 06:34:14 +0300

    Implement terminal history

--- Views/Terminal.axaml.cs
+++ Views/Terminal.axaml.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -10,7 +12,71 @@ namespace photocon.Views;
 
 public partial class Terminal : UserControl
 {
+    public class DropOutDistinctStack<T> : List<T> where T : IEquatable<T>
+    {
+        private readonly int _capacity;
+        protected int _peekIndex = 0;
+
+        public DropOutDistinctStack(int capacity)
+        {
+            _capacity = capacity;
+        }
+
+        public void Push(T item)
+        {
+            if (this.FirstOrDefault() != null)
+            {
+                if (item.Equals(this.First()))
+                {
+                    return;
+                }
+            }
+
+            if (this.Count >= _capacity)
+            {
+                this.RemoveAt(Count - 1);
+            }
+
+            Add(item);
+        }
+
+        public T? PeekNext()
+        {
+            if (_peekIndex >= Count) 
+            {
+                _peekIndex = Count;
+                return (this.LastOrDefault() == null) ? default : this.Last();
+            }
+            else if (_peekIndex < 0)
+            {
+                _peekIndex = 0;
+            }
+            return this[_peekIndex++];
+        }
+
+        public T? PeekPrevious()
+        {
+            if (_peekIndex >= Count)
+            {
+                _peekIndex = Count;
+                return (this.LastOrDefault() == null) ? default : this.Last();   
+            }
+            else if (_peekIndex < 0)
+            {
+                _peekIndex = 0;
+            }
+            if (_peekIndex == 0) return default;
+            return this[--_peekIndex];
+        }
+
+        public void ResetPeek()
+        {
+            _peekIndex = 0;
+        }
+    }
+
     protected TerminalViewModel? LastDataContext = null;
+    protected DropOutDistinctStack<string> History = new(100);
 
     public Terminal()
     {
@@ -20,6 +86,8 @@ public partial class Terminal : UserControl
 
     protected void Send_Click(object? sender, RoutedEventArgs e)
     {
+        History.Push(txtInput.Text);
+        History.ResetPeek();
         (DataContext as TerminalViewModel)?.RequestSending();
     }
 
@@ -38,8 +106,21 @@ public partial class Terminal : UserControl
 
     protected void TextBox_KeyDown(object? sender, KeyEventArgs e)
     {
-        if (e.Key != Key.Enter) return;
-        Send_Click(this, new RoutedEventArgs());
-        txtInput.Text = string.Empty;
+        string? p = null;
+        switch (e.Key)
+        {
+            case Key.Enter:
+                Send_Click(this, new RoutedEventArgs());
+                txtInput.Text = string.Empty;
+                break;
+            case Key.Up:
+                p = History.PeekNext();
+                break;
+            case Key.Down:
+                p = History.PeekPrevious();
+                break;
+            default: return;
+        }
+        if (p != null) txtInput.Text = p;
     }
 }
